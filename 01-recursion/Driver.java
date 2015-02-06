9f1ac7001aa309d1f5001f9f86c71f5c99258d28
daltonwu <dalton.z.wu@gmail.com>
2015-02-06 08:41:49 -0500

    Update

--- 01-recursion/Driver.java
+++ 01-recursion/Driver.java
@@ -1,7 +1,19 @@
 public class Driver {
+	// class problems +
 	// various codingbat problems
 	public static void main(String[] args) {
 		System.out.println("Whatever");
+		System.out.println(new Driver().count("hello", 'l'));
+	}
+	
+	public int len(String s) {
+		if(s.equals("")) return 0;
+		else return 1+len(s.substring(1));
+	}
+	public int count(String s, char c) {
+		if(s.length()==0) return 0;
+		if(s.charAt(0)==c) return 1+count(s.substring(1), c);
+		return count(s.substring(1),c);
 	}
 	
 	public int fibonacci(int n) {
@@ -23,12 +35,6 @@ public class Driver {
 	public int sumDigits(int n) {
 		if(n<10) return n;
 		return n%10 + sumDigits(n/10);
-	}
-	public int bunnyEars2(int bunnies) {
-		if(bunnies==0) return 0;
-		if(bunnies==1) return 2;
-		if(bunnies%2==0) return 3 + bunnyEars2(bunnies-1);
-		return 2 + bunnyEars2(bunnies-1);
 	}
 	public String allStar(String str) {
 		if(str.length()<=1) return str;
