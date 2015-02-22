5a1a83476574522da9ee50842beeb38c455c5aed
daltonwu <dalton.z.wu@gmail.com>
2015-02-21 21:09:50 -0500

    Update

--- 00-ugh/Driver.java
+++ 00-ugh/Driver.java
@@ -2,19 +2,24 @@ import java.util.ArrayList;
 
 public class Driver {
 	private ArrayList<Integer> fibArr;
+	private ArrayList<String> scrambled;
 	
-	public Driver {
+	public Driver() {
 		fibArr = new ArrayList<Integer>();
-		// 1,1,2,3,…
-		fibArr.set(0,1);
-		fibArr.set(1,1);
+		// (0),1,1,2,3,…
+		fibArr.add(0);
+		fibArr.add(1);
+		fibArr.add(1);
 	}
 	
 	public static void main(String[] args) {
 		Driver driverception = new Driver();
 		System.out.println(driverception.factorial(5)+"\n");
-		System.out.println(driverception.fib(0)+"\n");
-		System.out.println(driverception.sqrt(49.0));
+		for(int i=0; i<10; i++) {
+			System.out.println(driverception.fib(i)+"\n");
+		}
+		System.out.println(driverception.sqrt(49.0)+"\n");
+		System.out.println(driverception.scramble("abc").toString());
 	}
 	
 	public int factorial(int n) {
@@ -25,8 +30,8 @@ public class Driver {
 	public int fib(int n) {
 		// changed the list parameter to an instance variable
 		// because it didn't quite make sense the other way
-		if(fibArr.length() < n) {
-			fibArr.add(fibArr.get(fibArr.length()-1) + fibArr.get(fibArr.length()-2));
+		if(fibArr.size() <= n) {
+			fibArr.add(fibArr.get(fibArr.size()-1) + fibArr.get(fibArr.size()-2));
 		}
 		else return fibArr.get(n);
 		return fib(n);
@@ -44,6 +49,20 @@ public class Driver {
 		else return sqrtH(n, (n/guess+guess)/2, maxDiff);
 	}
 	
-	
-	
+	public ArrayList<String> scramble(String s) {
+		// I'm going to pretend duplicate letters are distinguishable
+		scrambled = new ArrayList<String>();
+		scrambleH("",s);
+		ArrayList<String> foo = new ArrayList<String>();
+		for(int i=0; i<scrambled.size(); i+=2) {
+			foo.add(scrambled.get(i));
+		}
+		return foo;
+	}
+	public void scrambleH(String one, String two) {
+		if(two.length()<=1) scrambled.add(one + two);
+		for(int i=0; i<two.length(); i++) {
+			scrambleH(one + two.charAt(i), two.substring(0,i) + two.substring(i+1));
+		}
+	}
 }
