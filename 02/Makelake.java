f67a9143b03ce3024a66e12d1bc6584c47387ff2
daltonwu <dalton.z.wu@gmail.com>
2015-02-24 15:29:45 -0500

    Update

--- 02/Makelake.java
+++ 02/Makelake.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.PrintWriter;
 
 public class Makelake {
 	// R, C, E, N
 	private static int[][] land;
 	private static int R, C, E, N;
+	private static int X, Y, D;
 	
 	public static void main(String[] args) {
 		Scanner input = null;
@@ -29,6 +31,42 @@ public class Makelake {
 		
 		while(input.hasNext()) {
 			String[] line = input.nextLine().split(" ");
+			X = Integer.parseInt(line[0]);
+			Y = Integer.parseInt(line[1]);
+			D = Integer.parseInt(line[2]);
+			
+			int max = -1;
+			for(int i=X-1; i<X+2; i++) {
+				for(int g=Y-1; g<Y+2; g++) {
+					max = Math.max(max, land[i][g]);
+				}
+			}
+			
+			for(int n=D; n>0; n--) {
+				for(int i=X-1; i<X+2; i++) {
+					for(int g=Y-1; g<Y+2; g++) {
+						if(land[i][g] == max)
+							land[i][g]--;
+					}
+				}
+				max--;
+			}
 		}
+		
+		int sum = 0;
+		
+		for(int r=0; r<R; r++) {
+			for(int c=0; c<C; c++) {
+				if(land[r][c] < E)
+					sum += E - land[r][c];
+			}
+		}
+		
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter("makelake.out");
+		} catch(Exception e) {}
+		pw.println(sum * 72 * 72);
+		pw.close();
 	}
 }
