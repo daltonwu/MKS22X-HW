c68d314e2ee19668e0b7935fcc7aca19cce3ef41
daltonwu <dalton.z.wu@gmail.com>
2015-03-19 15:35:36 -0400

    Update

---
+++ 08-test review 2/WaitingList.java
@@ -0,0 +1,42 @@
+public class WaitingList
+{
+	private int numNodes;
+	private ListNode front;
+	
+	// returns the number of nodes in this list
+	public int size()
+	{  return numNodes;  }
+	
+	
+	// returns a reference to the node at index k,
+	// where the indexes are numbered 0 through size()-1
+	// precondition: 0 <= k < size()
+	private ListNode getKthNode(int k)
+	{  /* to be implemented in part (a) */
+		if(k<0 || k>size()) {
+			System.out.println("You LIED!");
+			return null;
+		}
+		
+		ListNode next = front;
+		while(k>0) {
+			next = next.getNext();
+			k--;
+		}
+		
+		return next;
+	}
+	
+	
+	// removes the last num nodes from other and attaches them
+	// in the same order to the end of this WaitingList;
+	// updates the number of nodes in each list to reflect the move
+	// precondition:  size() > 0;
+	//                0 < num <= other.size()
+	public void transferNodesFromEnd(WaitingList other, int num)
+	{  /* to be implemented in part (b) */  
+		if(
+	}
+	
+	// There may be fields, constructors, and methods that are not shown.
+}
