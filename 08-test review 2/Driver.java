894f3e9b27b421c16f4fd5074438a99262e8c57e
daltonwu <dalton.z.wu@gmail.com>
2015-03-19 20:33:17 -0400

    URGH

---
+++ 08-test review 2/Driver.java
@@ -0,0 +1,16 @@
+public class Driver {
+	public static void main(String[] args) {
+		char foo;
+		//System.out.println(foo);
+		foo = 'a';
+		System.out.println(foo);
+		
+		char bar;
+		//System.out.println(bar);
+		bar = 2026;
+		System.out.println(bar);
+		
+		char omg = '…';
+		System.out.println(omg);
+	}
+}
