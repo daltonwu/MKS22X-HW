ce2de1b830f61b9647ece66de21cf33a97cd0eac
daltonwu <dalton.z.wu@gmail.com>
2015-04-22 14:59:00 -0400

    Update

--- 16-BinarySearchTree/Driver.java
+++
@@ -1,7 +0,0 @@
-public class Driver {
-	public static void main(String[] args) {
-		BinarySearchTree<Double> bst = new BinarySearchTree<>();
-		Double[] values = [7, -5, Math.E, 1, 1.2, sqrt(21), 12, 7.3, 9, 54, 20];
-
-	}
-}
