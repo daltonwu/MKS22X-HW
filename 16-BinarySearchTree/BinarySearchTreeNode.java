a916c3b9d05953039a3b38b1b1a164b221fafdf8
daltonwu <dalton.z.wu@gmail.com>
2015-04-22 15:11:41 -0400

    Update

--- 16-BinarySearchTree/BinarySearchTreeNode.java
+++ 16-BinarySearchTree/BinarySearchTreeNode.java
@@ -1,38 +1,32 @@
-import java.io.*;
-import java.util.*;
-
-public class BSTreeNode<T extends Comparable> {
-
-    private T data;
-    private BSTreeNode<T> left;
-    private BSTreeNode<T> right;
-
-    public BSTreeNode( T d ) {
- 
-	data = d;
-	left = right = null;
-    }
-    
-    //accessors
-    public T getData() {
-	return data;
-    }
-    public BSTreeNode<T> getLeft() {
-	return left;
-    }
-    public BSTreeNode<T> getRight() {
-	return right;
-    }
-
-    //mutators
-    public void setData( T d ) {
-	data = d;
-    }
-    public void setLeft( BSTreeNode<T> l ) {
-	left = l;
-    }
-    public void setRight( BSTreeNode<T> r ) {
-	right = r;
-    }
+public class BinarySearchTreeNode<T extends Comparable> {
+	private T data;
+	private BinarySearchTreeNode<T> left, right;
+	
+	public BinarySearchTreeNode(T d) {
+		data = d;
+		left = right = null;
+	}
+	
+	// getters …
+	public T getData() {
+		return data;
+	}
+	public BinarySearchTreeNode<T> getLeft() {
+		return left;
+	}
+	public BinarySearchTreeNode<T> getRight() {
+		return right;
+	}
+	
+	// … 'n' setters
+	public void setData(T d) {
+		data = d;
+	}
+	public void setLeft(BinarySearchTreeNode<T> l) {
+		left = l;
+	}
+	public void setRight(BinarySearchTreeNode<T> r) {
+		right = r;
+	}
 }
 
