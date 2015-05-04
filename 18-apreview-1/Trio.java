8900e446e1dd792208f701be913efe79d8e8b5b4
daltonwu <dalton.z.wu@gmail.com>
2015-05-04 14:53:01 -0400

    Update

---
+++ 18-apreview-1/Trio.java
@@ -0,0 +1,19 @@
+public class Trio implements MenuItem {
+	Sandwich sandwich;
+	Salad salad;
+	Drink drink;
+	
+	public Trio(Sandwich sandwich, Salad salad, Drink drink) {
+		this.sandwich = sandwich;
+		this.salad = salad;
+		this.drink = drink;
+	}
+	
+	public String getName() {
+		return sandwich.getName() + "/" + salad.getName() + drink.getName() + "/" + " Trio";
+	}
+	
+	public double getPrice() {
+		return sandwich.getPrice() + salad.getPrice() + drink.getPrice() - Math.min(sandwich.getPrice(), Math.min(salad.getPrice(), drink.getPrice()));
+	}
+}
